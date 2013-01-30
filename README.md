7a3ee06f68342f8b737cfaa099c5d1d918590b31
Giles <gilesbradshaw0@gmail.com>
2013-01-30 00:32:34 +0000

    Added javascript file

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Supports native and jQuery templates.
 The Knockout.js Observable Template Engine extends Knockout.js to allow you to load templates from obsrvables in the view model. 
 It currently supports both native and jquery templates.
 
+It also allows your templates to be SVG elements (use the svg option)
+
 ## Prerequisites
 Knockout.js 2.0 or later (you will need to look at the tagged 1.0 version if you need support for older Knockout.js)
 jQuery 1.5 or later
@@ -19,7 +21,16 @@ By referencing koObservableTemplateEngine.js, you've automatically overridden th
 To bind to a template from your view model do
     <div data-bind="template: { name: 'aname', template:myTemplate }"></div>
 
+You need to give the template a name but this can be any name.
+
 Where myTemplate binds to an observable in the view model and aname is not the name of a template.
+
+If your template contains SVG elements ie elements below the root svg element itself then you need to use the svg option.
+    <div data-bind="template: {svg:true, name: 'aname', template:myTemplate }"></div>
+
+If you want the template to be a normal named template - ie the name indicates the id of an element in your DOM then miss out the template option.
+    <div data-bind="template: { name: 'aname'}"></div>
+
 
 ## Thanks to..
 Derived from the [ifandelse / Knockout.js-External-Template-Engine] (https://github.com/ifandelse/Knockout.js-External-Template-Engine)
---
+++ koObservableTemplateEngine.js
@@ -0,0 +1,121 @@
+
+// Knockout Observable Template Engine
+// Author: Giles Bradshaw
+// License: MIT (http://www.opensource.org/licenses/mit-license)
+// Version 0.0.1
+// unashamably based on https://github.com/ifandelse/Knockout.js-External-Template-Engine/blob/master/src/ExternalTemplateSource.js
+
+
+define(['knockout', 'jquery'], function (ko, jQuery) {
+
+    var ObservableTemplateSource = function (templateId, options) {
+        var self = this, origAfterRender;
+        self.templateId = templateId;
+        self.loaded = false;
+        self.template = ko.observable();
+        self.template.data = {};
+        self.options = ko.utils.extend({}, options);
+        self.options.templateId = templateId;
+        if (self.options && self.options.afterRender) {
+            origAfterRender = self.options.afterRender;
+            self.options.afterRender = function () {
+                if (self.loaded) {
+                    origAfterRender.apply(self.options, arguments);
+                }
+            }
+        }
+    };
+
+    ko.utils.extend(ObservableTemplateSource.prototype, {
+        data: function (key, value) {
+            if (arguments.length === 1) {
+                if (key === "precompiled") {
+                    this.template();
+                }
+                return this.template.data[key];
+            }
+            this.template.data[key] = value;
+        },
+
+        text: function (value) {
+            if (!this.loaded) {
+                if (this.options.template) {
+                    this.template(ko.utils.unwrapObservable(this.options.template));
+                    if (ko.isObservable(this.options.template)) {
+                        var _this = this;
+                        this.options.template.subscribe(function (data) { _this.template(data); });
+                    }
+       
+                }
+                this.loaded = true;
+
+            }
+
+            if (arguments.length === 0) {
+                return this.template();
+            } else {
+                this.template(arguments[0]);
+            }
+        }
+    });
+    
+    var KoObservableTemplateEngine = function (koEngineType) {
+        var engine = koEngineType ? new koEngineType() : new ko.nativeTemplateEngine();
+        engine.cachedTemplates ={};
+        var cacheId = 0;
+        engine.makeTemplateSource = function (template, bindingContext, options) {
+            // Named template
+            if (typeof template == "string") {
+                
+                var elem = document.getElementById(template) && !options.template;
+                if (elem)
+                    return new ko.templateSources.domElement(elem);
+                else {
+                    //no cache means don't cache the template by the name
+                    if (!template.cacheId) {
+                        template.cacheId = "cache" + cacheId++;
+                        engine.cachedTemplates[template.cacheId] = new ObservableTemplateSource(template, options);
+                    }
+                    return engine.cachedTemplates[template.cacheId];
+                }
+            }
+            else if ((template.nodeType == 1) || (template.nodeType == 8)) {
+                // Anonymous template
+                return new ko.templateSources.anonymousTemplate(template);
+            }
+
+        };
+
+        engine.renderTemplate = function (template, bindingContext, options) {
+            var templateSource = engine.makeTemplateSource(template, bindingContext, options);
+            if (!options.svg)
+                return engine.renderTemplateSource(templateSource, bindingContext, options);
+            else {
+                var doc = new window.DOMParser().parseFromString(
+                   '<svg xmlns="http://www.w3.org/2000/svg"><g>' + templateSource.text() + '</g></svg>',
+                   'application/xml');
+                doc = document.importNode(doc.documentElement, true);
+                var ret = [];
+                var child = doc.firstChild;
+                while (child) {
+                    ret.push(child);
+
+                    child = child.nextSibling;
+                }
+                return ret;
+            }
+        };
+
+        return engine;
+    };
+
+    ko.KoObservableTemplateEngine = KoObservableTemplateEngine;
+
+    if (jQuery['tmpl'] && jQuery['tmpl']['tag']['tmpl']['open'].toString().indexOf('__') >= 0) {
+        ko.setTemplateEngine(new KoObservableTemplateEngine(ko.jqueryTmplTemplateEngine));
+    }
+    else {
+        ko.setTemplateEngine(new KoObservableTemplateEngine());
+    }
+
+});
